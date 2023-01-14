063de0095df97785e6815a63b80ca5a08cc75056
HookSandeer <hooksander@proton.me>
2023-01-15 00:40:37 +0100

    Add import function + commit all the code

--- script.py
+++ script.py
@@ -2,22 +2,43 @@
 #! By HookSander
 
 from PIL import Image
-import time
+
 
 def importPicture(path) :
+    """
+    Args:
+        path (str): Chemin d'accès' de l'image
+
+    Returns:
+        PIL Image : L'image que nous allons convertir
+    """
     return Image.open(path)
 
+
 def resizePic(pic) :
+    """
+    Args:
+        pic (PIL Image): L'image original
+
+    Returns:
+        PIL Image: La même mais redimensioné en 480p, 16/9
+    """
     return pic.resize((854, 480))
+
+
+def getPixel(pic) :
+    print(pic.size)
 
 
     
 
 if __name__ == '__main__' :
-    picturePath = input("Enter your Picture emplacement path\n=>")
+    
+    picturePath = input("Entrer le chemin d'accès relatif de votre image\n=>")     #?# Récupération du chemin d'accès relatif 
     try :
-        newPicture = resizePic(importPicture(picturePath))
-    except :
-        print("Error in your picture path, please try again.")
-        exit()
+        newPicture = resizePic(importPicture(picturePath))              #?# ###
+    except :                                                            #?#   Prévention d'erreur
+        print("Erreur durant le chargement de l'image")                 #?#  Arret si erreur
+        exit()                                                          #?# ###
+    
     

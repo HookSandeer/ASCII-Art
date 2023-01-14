8f1b1a6bf4d1c29bab745e57a23f6bec3b1e285d
HookSandeer <hooksander@proton.me>
2023-01-15 00:27:57 +0100

    Add open function

--- script.py
+++ script.py
@@ -1,2 +1,23 @@
 
-# By HookSander
+#! By HookSander
+
+from PIL import Image
+import time
+
+def importPicture(path) :
+    return Image.open(path)
+
+def resizePic(pic) :
+    return pic.resize((854, 480))
+
+
+    
+
+if __name__ == '__main__' :
+    picturePath = input("Enter your Picture emplacement path\n=>")
+    try :
+        newPicture = resizePic(importPicture(picturePath))
+    except :
+        print("Error in your picture path, please try again.")
+        exit()
+    

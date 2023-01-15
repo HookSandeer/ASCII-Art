f09338435b4134c09e3720d66b268fa2ad50589f
HookSandeer <hooksander@proton.me>
2023-01-15 12:52:10 +0100

    Add Graphic image import

--- script.py
+++ script.py
@@ -1,11 +1,19 @@
 
 #! By HookSander
 
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 from PIL import Image
 import time
 
 asciiChar =  " .'`^,:;Il!i><~+_-?][}{1)(|\/tfjrnxuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@"
 
+def locatePicture() :
+    win = Tk()
+    win.withdraw()
+    path = askopenfilename()
+    return path
 
 def importPicture(path) :
     return Image.open(path)
@@ -20,20 +28,21 @@ def monochrome(pic) :
     return pic.convert("L")
 
 
+asciiChar =  " .'`^,:;Il!i><~+_-?][}{1)(|\/tfjrnxuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@"
+#asciiChar = " .^~:/[o%O@"
+#asciiChar = " .:/o0@"
+
 def convertAscii(pic, char) :
     for y in range(144) :
         currentLine = ""
         for x in range(256) :
             pix = pic.getpixel((x, y))
-            sym = pix * 67 // 255
+            sym = (pix*len(char)//255)-1
             currentLine += char[sym] + " "
         print(currentLine)
-        time.sleep(0.1)
-
 
 if __name__ == '__main__' :
-    
-    picturePath = input("Entrer le chemin d'accès relatif de votre image\n=>")     #?# Récupération du chemin d'accès relatif 
+    picturePath = locatePicture()
     try :
         newPicture = resizePic(importPicture(picturePath))              #?# ###
     except :                                                            #?#   Prévention d'erreur

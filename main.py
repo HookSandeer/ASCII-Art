996588b5390dec34aaffe376487bea02214012f2
HookSandeer <hooksander@proton.me>
2023-01-21 00:45:33 +0100

    Update

--- main.py
+++ main.py
@@ -5,35 +5,40 @@ from PIL import Image, ImageFont, ImageDraw
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-def picToAscii() :
-    #listAscii = " .,:;*+?%$#@"
-    listAscii = " -oW#"
-    finalText = ""
+
+def chemin() :
     win = Tk()
     win.withdraw()
-    picturePath = askopenfilename()
-    pictureFile = Image.open(picturePath)
-    pictureResize = pictureFile.resize((160, 128))
-    pictureMono = pictureResize.convert("L")
-    
-    for y in range(128) :
-        for x in range(160) :
-            pixel = pictureMono.getpixel((x, y))
-            sym = (pixel*len(listAscii)//255)-1
-            finalText += listAscii[sym] + "  "
-        finalText += "\n"
-    
-    return finalText
+    return askopenfilename()
 
 
-def textToImage(text) :
-    charWidth = 8
-    charHeight = 18
-    img = Image.new("RGB", (charWidth*128, charHeight*160), (0, 0, 0))
-    image = ImageDraw.Draw(img)
-    fontType = ImageFont.truetype('DejaVuSerif.ttf', 3)
-    image.text((50, 50), text, (255, 255, 255), font=fontType)
-    img.save("AsciiArt.jpg")
-    
 
-textToImage(picToAscii())
+police = ImageFont.truetype('Lucon.ttf', 15)
+
+def caractere(num) :
+    asciiChar = " -oW@"
+    listChar = list(asciiChar)
+    return listChar[num*len(listChar)//256]
+
+
+echelleRedimension = 0.1
+largeurCaractere = 10
+hauteurCaractere = 18
+photo = Image.open(chemin())
+largeur, hauteur = photo.size
+image = photo.resize((int(echelleRedimension*largeur), int(echelleRedimension*hauteur)), Image.NEAREST)
+largeur, hauteur = image.size
+imageActuelle = image.load()
+
+imageSortie = Image.new('RGB', (largeurCaractere*largeur, hauteurCaractere*hauteur), color = (0, 0, 0))
+imageAjoutTexte = ImageDraw.Draw(imageSortie)
+
+
+for y in range(largeur) :
+    for x in range(hauteur) :
+        r, v, b = imageActuelle[x, y]
+        a = int(r/3 + v/3 + b/3)
+        imageActuelle[x, y] = (a, a, a)
+        imageAjoutTexte.text((x*largeurCaractere, y*hauteurCaractere), caractere(a), font = police, fill = (r, v, b))
+    
+imageSortie.save('Ascii.png')

a20fa99d3ff95797af580f3a5c5c53ccec7fa29b
HookSandeer <hooksander@proton.me>
2023-01-26 17:23:10 +0100

    Add main menu application

---
+++ tkintermenu.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# By HookSander
+
+#### IMPORTS ####
+from tkinter import Tk
+
+
+win = Tk()
+win.title("Générateur ASCII Art")
+win.geometry('1600x800')
+
+
+
+win.mainloop()
